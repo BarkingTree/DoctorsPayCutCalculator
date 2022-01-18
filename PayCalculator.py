8cc554c1764e80f5592844d4ddc1b49ce8ee0733
BarkingTree <73749523+BarkingTree@users.noreply.github.com>
2022-01-18 17:22:26 +0000

    added England JSON + Fetch API ONS Function

--- PayCalculator.py
+++ PayCalculator.py
@@ -6,56 +6,45 @@ import streamlit as st
 from PIL import Image
 import json 
 import requests
+from dateutil.relativedelta import relativedelta
 
+# Get RPI Data 
+def getRPI(yearSelected): 
+    response = requests.get('https://api.ons.gov.uk/timeseries/chaw/dataset/mm23/data') 
+    jsonData = response.json()
+    rpiYears = jsonData["years"]
+    induvidualYear = rpiYears[yearSelected - 1987]
+    rpi = induvidualYear["value"]
+    return rpi
 
-#ONS Query Functions
-def process(obj, ds):
-	data = {}
-	values  =  obj[ds]['value']
-	index = obj[ds]['dimension'][obj[ds]['dimension']['id'][1]]['category']['index']
-	labels = obj[ds]['dimension'][obj[ds]['dimension']['id'][1]]['category']['label']
-	for l in labels:
-		num = index[l]
-		count = values[str(num)]
-		data[labels[l]] = count
-	return data
-
-def getData(): 
-    response = requests.get('https://api.beta.ons.gov.uk/v1/datasets/cpih01') 
-    if response.getcode() == 200:
-        source = response.read()
-        data = json.loads(source)
-        return data
-    else:
-        print('An error occurred while attempting to retrieve data from the API.')
-
-response = requests.get('https://api.ons.gov.uk/timeseries/chaw/dataset/mm23/data') 
-
-print(response.json())
-print('response above')
-
-st.title('Medics For Pay Restoration')
-st.subheader('Calculate Your Pay Cut - England')
-st.json(response.json())
-
+with st.container():
+    st.title('Medics For Pay Restoration')
+    st.subheader('Calculate Your Pay Cut - England')
+    
 from datetime import date
 currentDate = date.today()
 st.subheader('Year')
-slider_time = st.slider(
+slider_year_selected = st.slider(
      "Select Year to Compare to",
      min_value= date(2008, 1, 1),
-     max_value= currentDate,
+     max_value= currentDate - relativedelta(years= 1, days=19),
+     # Change to month = 1 on release
      format="YYYY")
-formattedTime = slider_time.strftime("%d/%m/%Y")
+
 
 st.subheader('Your Grade')
 grade = st.selectbox(
      'Select Your Grade',
      ('FY1', 'FY2', 'ST1', 'ST2', 'ST3', 'ST4', 'ST5', 'ST6', 'ST7', 'ST8'))
 st.write('Grade:', grade)
-st.subheader('Antisocial Commitment')
-antisocialHours = st.slider('Number of Antisocial Hours Worked?', 0, 20, 0)
+st.subheader('Antisocial Hours')
+antisocialHoursOld = st.slider('Hours worked outside of Monday - Friday 07:00 - 19:00 ?', 0, 20, 0)
+antisocialHoursNew = st.slider('Of these how many are outside of Monday - Friday 07:00 - 21:00 ?', 0, 20, 0)
+
 st.subheader('Weekends Worked')
 weekendsWorked = st.select_slider(
-    '<1:8 = Work One in Eight', 
+    '<1:8 = Work One in Eight Weekends', 
     options=[ '<1:8', '<1:7 - 1:8', '<1:6 - 1:7', '<1:5 - 1:6', '<1:4 - 1:5', '<1:3 - 1:4', '<1:2 - 1:3', '1:2',])
+
+st.write(getRPI(slider_year_selected.year))
+
