ae31e9f9e6b92d3d86b30427411b0e12a13b2dc0
BarkingTree <73749523+BarkingTree@users.noreply.github.com>
2022-01-19 21:52:12 +0000

    Changed date which new contract calculation applie

--- PayCalculator.py
+++ PayCalculator.py
@@ -1,10 +1,12 @@
 from ctypes import alignment
 from nis import match
+from pickle import TRUE
 from tkinter import CENTER
 from turtle import color
 from unicodedata import decimal
 from unittest import case
 from urllib import response
+from nbformat import write
 from numpy import inf
 from pandas import json_normalize
 import streamlit as st
@@ -20,7 +22,6 @@ additionalHourEnhancement = 0.025
 nrocMultiplier = 0.08
 ltft = False
 
-@st.cache
 def weekendAllowance(yearSelected): 
     response = requests.get('https://raw.githubusercontent.com/BarkingTree/PythonPayCalculator/master/englandPay.json') 
     jsonData = response.json()
@@ -93,7 +94,7 @@ def currentPay(yearSelected, gradeSelected, nroc, ltft, weekendsWorked = '<1:8')
         
         additionalHoursPay = 0
         antisocialPay = basePay * (antisocialHours * antiocialEnhancementPerHour) 
-        weekendPay = (basePay * weekendMultiplier) * percentageWorked    
+        weekendPay = (basePay * (weekendMultiplier / 100 )) * percentageWorked    
         totalPayRaw = ltftPay + antisocialPay + weekendPay + nrocPay + ltftAllowance
         totalPayRounded = round(totalPayRaw)
         resultsArray = [totalPayRounded, ltftPay, antisocialPay, additionalHoursPay, weekendPay, nrocPay, ltftAllowance]
@@ -102,34 +103,34 @@ def currentPay(yearSelected, gradeSelected, nroc, ltft, weekendsWorked = '<1:8')
          # Caluclate Pay if > 40 Hours Per Week
         # Unable to Use Match due to Streamlit Requiring Python 3.9 or Older. Review if Streamlit adds support for Python 3.10
         if weekendsWorked == '<1:8': 
-            weekendMultiplier = 0
+            weekendMultiplier = weekAllowance[0]
         if weekendsWorked == '<1:7 - 1:8':
-            weekendMultiplier = 0.03
+            weekendMultiplier = weekAllowance[1]
         if weekendsWorked == '<1:6 - 1:7': 
-            weekendMultiplier = 0.04
+            weekendMultiplier = weekAllowance[2]
         if weekendsWorked == '<1:5 - 1:6': 
-            weekendMultiplier = 0.05
+            weekendMultiplier = weekAllowance[3]
         if weekendsWorked == '<1:4 - 1:5': 
-            weekendMultiplier = 0.06
+            weekendMultiplier = weekAllowance[4]
         if weekendsWorked == '<1:3 - 1:4': 
-            weekendMultiplier = 0.075
+            weekendMultiplier = weekAllowance[5]
         if weekendsWorked == '<1:2 - 1:3': 
-            weekendMultiplier = 0.10
+            weekendMultiplier = weekAllowance[6]
         if weekendsWorked == '1:2':
-            weekendMultiplier = 0.15
+            weekendMultiplier = weekAllowance[7]
         if nroc: 
             nrocPay = nrocMultiplier * basePay
         else:
             nrocPay = 0
         antisocialPay = basePay * (antisocialHours * antiocialEnhancementPerHour) 
         additionalHoursPay = basePay * ((hoursWorked - 40) * additionalHourEnhancement)
-        weekendPay = basePay * weekendMultiplier
+        weekendPay = basePay * (weekendMultiplier / 100)
         totalPayRaw = basePay + additionalHoursPay + antisocialPay + weekendPay + nrocPay
         totalPayRounded = round(totalPayRaw)
         resultsArray = [totalPayRounded, basePay, antisocialPay, additionalHoursPay, weekendPay, nrocPay, 0]
         return resultsArray
 
-def oldPay(yearSelected, gradeSelected, hours, antiSocialHours, ltft, weekendsWorked = '<1:8', ): 
+def oldPay(yearSelected, gradeSelected, hours, antiSocialHours, ltft, weekendsWorked): 
     percentAntiSocialHours = antiSocialHours / hours
     basePay = getPayData(yearSelected, gradeSelected)
     resultsArray = []
@@ -149,40 +150,65 @@ def oldPay(yearSelected, gradeSelected, hours, antiSocialHours, ltft, weekendsWo
             baseSalaryBanding = 0.8
         if percentageWorked in range(90, 99): 
             baseSalaryBanding = 0.9             
-        if weekendsWorked == '<1:8' or '<1:7 - 1:8':
-           worksOneinSixWeekends = False
-        if weekendsWorked == '<1:6 - 1:7' or '<1:5 - 1:6' or '<1:4 - 1:5' or '<1:3 - 1:4' or '<1:2 - 1:3' or '1:2':  
-            worksOneinFourWeekends = True
+        if weekendsWorked == '<1:8': 
+            worksOneinSixWeekends = False
+        if weekendsWorked == '<1:7 - 1:8':
+            worksOneinSixWeekends = False
+        if weekendsWorked == '<1:6 - 1:7': 
+            worksOneinSixWeekends = False
+        if weekendsWorked == '<1:5 - 1:6': 
+           worksOneinSixWeekends = True
+        if weekendsWorked == '<1:4 - 1:5': 
+            worksOneinSixWeekends = True
+        if weekendsWorked == '<1:3 - 1:4': 
+            worksOneinSixWeekends = True
+        if weekendsWorked == '<1:2 - 1:3': 
+            worksOneinSixWeekends = True
+        if weekendsWorked == '1:2':
+           worksOneinSixWeekends = True
         if worksOneinSixWeekends or percentAntiSocialHours > 0.33: 
             #Band FA
             banding = 1.5
-        else:
+        elif percentAntiSocialHours > 0:
             #Band FB 
             banding = 1.4
+        elif percentAntiSocialHours == 0: 
+            banding = 1.2
         bandedBasePay = basePay * baseSalaryBanding
         totalPayRaw = bandedBasePay * banding
         totalPayRounded = round(totalPayRaw)
         resultsArray = [totalPayRounded, round(bandedBasePay), banding]
-        print(percentageWorked)
-        print(baseSalaryBanding)
     
     if ltft == False:
-        worksOneinFourWeekends = False
-        if weekendsWorked == '<1:8' or '<1:7 - 1:8' or '<1:6 - 1:7' or '<1:5 - 1:6' or '<1:4 - 1:5':
-           worksOneinFourWeekends = False
-        if weekendsWorked == '<1:3 - 1:4' or '<1:2 - 1:3' or '1:2':  
-            worksOneinFourWeekends = True
-        if worksOneinFourWeekends or percentAntiSocialHours > 0.33: 
+        worksOneInFourWeekends = False
+        if weekendsWorked == '<1:8': 
+            worksOneInFourWeekends = False
+        if weekendsWorked == '<1:7 - 1:8':
+            worksOneInFourWeekends = False
+        if weekendsWorked == '<1:6 - 1:7': 
+            worksOneInFourWeekends = False
+        if weekendsWorked == '<1:5 - 1:6': 
+           worksOneInFourWeekends = False
+        if weekendsWorked == '<1:4 - 1:5': 
+            worksOneInFourWeekends = False
+        if weekendsWorked == '<1:3 - 1:4': 
+            worksOneInFourWeekends = True
+        if weekendsWorked == '<1:2 - 1:3': 
+            worksOneInFourWeekends = True
+        if weekendsWorked == '1:2':
+           worksOneInFourWeekends = True
+        if worksOneInFourWeekends or percentAntiSocialHours > 0.33: 
             #Band 1A
             banding = 1.5
-        else:
+        elif percentAntiSocialHours > 0:
             #Band 1B 
             banding = 1.4
-
+        elif percentAntiSocialHours == 0: 
+            # Band 1C. Likely to require rework
+            banding = 1.2
         totalPayRaw = basePay * banding
         totalPayRounded = round(totalPayRaw)
         resultsArray = [totalPayRounded, round(basePay) ,banding]
-    
     return resultsArray
 
 with st.container():
@@ -214,7 +240,7 @@ hoursWorked = st.slider('Total Hours Worked Per Week', 20, 48, 40)
 if hoursWorked < 40:
     ltft = True 
 antisocialHours = st.slider('Hours Worked Outside of 07:00 - 21:00 (2016 Contract Antisocial)?', 0, 20, 0)
-antisocialHoursOld = st.slider('Hours Worked Outside of 07:00 - 07:00 (2002 Contract Antisocial)?', 0, 20, 0)
+antisocialHoursOld = st.slider('Hours Worked Outside of 07:00 - 19:00 (2002 Contract Antisocial)?', 0, 20, 0)
 st.write('Do you recieve the Non-Resident On Call Supplement?')
 nroc = st.checkbox('Non-Resident On Call')
 
@@ -230,37 +256,60 @@ currentInflation = getRPI(adjustedDate.year, inflationMeasure)
 selectedInflation = getRPI(slider_year_selected.year, inflationMeasure)
 inflationChange = float(currentInflation) - float(selectedInflation)
 payArray = currentPay(adjustedDate.year, grade, nroc, ltft ,weekendsWorked)
-payArrayOld = oldPay(slider_year_selected.year, grade, hoursWorked, antisocialHoursOld, ltft, weekendsWorked)
+if slider_year_selected < date(2017, 1, 1): 
+    payArrayOld = oldPay(slider_year_selected.year, grade, hoursWorked, antisocialHoursOld, ltft, weekendsWorked)
+elif slider_year_selected > date(2017, 1, 1):
+    payArrayOld = currentPay(slider_year_selected.year, grade, nroc, ltft ,weekendsWorked)
 inflationPercentage = float(currentInflation) / float(selectedInflation)
 inflationPercentageDisplay = round(inflationPercentage * 100)
 oldPayWithInflation = payArrayOld[0] * inflationPercentage
 
 relativePayLoss = round(payArray[0] - oldPayWithInflation)
 percentageLoss = round(relativePayLoss / payArray[0] * 100)
+change = ""
+if oldPayWithInflation < payArray[0]: 
+    change = "+Gain"
+elif oldPayWithInflation > payArray[0]:
+    change = "-Loss"
 
 col1, col2, col3 = st.columns(3)
 with col1:
-    st.metric('Pay Loss', f'£{relativePayLoss}', percentageLoss)
+    st.metric('Pay Loss', f'£{relativePayLoss}', change)
 with col2:
-     st.metric('2008 Pay Adjusted For Inflation', f'£{round(oldPayWithInflation)}')
+     st.metric(f'{slider_year_selected.year} Pay Adjusted For Inflation', f'£{round(oldPayWithInflation)}')
 with col3: 
-     st.metric(f'{adjustedDate.year}', f'£{payArray[0]}', percentageLoss)
+     st.metric(f'{adjustedDate.year}', f'£{payArray[0]}', f'{percentageLoss}%')
 col1, col2 = st.columns(2)
+
 with col1:
-    st.subheader(f'{slider_year_selected.year}')
-    st.subheader(f'Your Pay: £{payArrayOld[0]}')
-    st.write(f'Base Pay: £{payArrayOld[1]}')
-    st.write(f'Your Banding: {payArrayOld[2]}')
+    if slider_year_selected < date(2017, 1, 1): 
+        st.subheader(f'{slider_year_selected.year}')
+        st.subheader(f'Your Pay: £{payArrayOld[0]}')
+        st.write(f'Base Pay: £{payArrayOld[1]}')
+        st.write(f'Your Banding: {payArrayOld[2]}')
+        st.write('Based of the 2002 Contract')
+    if slider_year_selected > date(2017, 1, 1):
+        st.subheader(f'{slider_year_selected.year}')
+        st.subheader(f'Your Pay: £{round(payArrayOld[0])}')
+        st.write(f'Base Pay: £{round(payArrayOld[1])}')
+        st.write(f'Antisocial Hours Supplement: £{round(payArrayOld[2])}')
+        if ltft == False: 
+            st.write(f'Supplement for > 40 Hours Per Week £{round(payArrayOld[3])}')
+            st.write(f'Weekend Supplement: £{round(payArrayOld[4])}')
+            st.write(f'Non Resident On Call Supplement: £{round(payArrayOld[5])}')
+            if ltft == True: 
+                st.write(f'Less Than Full Time Allowance: £{round(payArrayOld[6])}')
+            
 
 with col2: 
     st.subheader(f'{adjustedDate.year}')
     st.subheader(f'Your Pay: £{payArray[0]}')
     st.write(f'Base Pay: £{payArray[1]}')
-    st.write(f'Antisocial Hours Supplement: £{payArray[2]}')
+    st.write(f'Antisocial Hours Supplement: £{round(payArray[2])}')
     if ltft == False: 
-        st.write(f'Supplement for > 40 Hours Per Week £{payArray[3]}')
-    st.write(f'Weekend Supplement: £{payArray[4]}')
-    st.write(f'Non Resident On Call Supplement: £{payArray[5]}')
+        st.write(f'Supplement for > 40 Hours Per Week £{round(payArray[3])}')
+    st.write(f'Weekend Supplement: £{round(payArray[4])}')
+    st.write(f'Non Resident On Call Supplement: £{round(payArray[5])}')
     if ltft == True: 
-        st.write(f'Less Than Full Time Allowance: £{payArray[6]}')
+        st.write(f'Less Than Full Time Allowance: £{round(payArray[6])}')
 
